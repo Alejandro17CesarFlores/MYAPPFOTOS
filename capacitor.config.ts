a4acbd21ae6fcabc470c7d39ea5249755c516203
Alejandro17CesarFlores <floreslopezcesaralejandro2001@gmail.com>
2024-10-12 12:52:06 -0600

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'MyAppFotos',
+  webDir: 'www'
+};
+
+export default config;
